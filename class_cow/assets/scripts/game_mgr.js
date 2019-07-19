55094054887251d2eb8b1040eef6af1a897f6532
薛龙 <xuelong9009@qq.com>
2019-07-19 18:18:58 +0800

    [项目创建]套牛小程序练习

---
+++ class_cow/assets/scripts/game_mgr.js
@@ -0,0 +1,19 @@
+//定义一个类，
+//组件类的基类
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    onLoad () {},
+
+    start () {
+
+    },
+
+    update (dt) {
+        console.log("开始输出");
+    },
+});
